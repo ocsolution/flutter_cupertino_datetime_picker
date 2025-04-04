d8bd47739d8794140f1a65f9e85ccb03061b5f06
ocsolution <100822599+ocsolution@users.noreply.github.com>
2025-04-04 08:19:57 +0700

    initialize Flutter project structure and add necessary configuration files

---
+++ example/lib/main.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_cupertino_datetime_picker/flutter_cupertino_datetime_picker.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        fontFamily: 'kmFont',
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              color: Colors.grey,
+              height: 400,
+              child: DateTimePickerWidget(
+                initDateTime: DateTime.now(),
+                maxDateTime: DateTime.now().add(Duration(days: 365)),
+                minDateTime: DateTime.now(),
+                dateFormat: "dd/MMM/yyyy HH:mm",
+                pickerTheme: const DateTimePickerTheme(
+                    cancel: SizedBox(), confirm: SizedBox(), titleHeight: 0),
+                locale: DateTimePickerLocale.km_kh,
+                onChange: (d, ds) {
+                  setState(() {});
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
